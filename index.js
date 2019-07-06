b4a7c4fa1ea3ed8340c3dc2b361be89f27700d1a
Harshith Patte <harshithpatte@gmail.com>
2019-07-07 01:41:44 +0530

    dynamic port

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var app = express();
-var port = process.env.port || 3000;
 var userProfileRoute = require("./UserProfileRoute.js");
 var bodyParser = require('body-parser')
 
 app.use(bodyParser.json());
 app.use('/api', userProfileRoute);
 
-app.listen(port);
+app.listen(process.env.PORT|| 3000);
