dc4c4848f22b7e35e273d897c87867b163791e3f
Harshith Patte <harshithpatte@gmail.com>
2019-07-07 01:18:16 +0530

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# login
+create login services
+1. using mongo and express
+2. using passport
---
+++ config.js
@@ -0,0 +1,3 @@
+const dbUrl = 'mongodb+srv://harshithpatte:ilovetocode@cluster0-7l21n.mongodb.net/test?retryWrites=true&w=majority';
+
+module.exports.dbUrl = dbUrl;
